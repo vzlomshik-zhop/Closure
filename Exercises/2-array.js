3f66839001ffcb08b7cc7dab99532124903180fe
vzlomshik-zhop <55409370+vzlomshik-zhop@users.noreply.github.com>
2020-01-06 14:58:55 +0200

    Update 2-array.js

--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,18 @@
 'use strict';
 
-const array = () => null;
+const array = () => {
+  const arr = [];
+  const get_val = (index) => arr[index];
+  get_val.push = (val) => {
+    arr.push(val);
+  }
+  get_val.pop = () => {
+    const a = arr[arr.length - 1];
+    if (arr.length > 0)
+      arr.length--;
+    return a;
+  }
+  return get_val;
+}
 
 module.exports = { array };
